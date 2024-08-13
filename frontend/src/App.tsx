3e1f30bffdc6e273c9aa7bf629457c028e21a5d5
A F <brunolloretfuentes@macbook-air.mynetworksettings.com>
2024-08-13 11:56:58 -0400

    Fixed tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-  )
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
+  );
 }
